7a936f5258fdf8c85b6894d618163417889520e8
AftaAufaddin <aftaaufaddin@gmail.com>
2025-01-01 18:21:18 +0700

    Add files via upload

---
+++ mahasiswa/Mahasiswa.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mahasiswa;
+
+/**
+ *
+ * @author ADVAN
+ */
+public class Mahasiswa {
+    public void membaca() {
+        System.out.println("Mahasiswa sedang membaca buku");
+    }
+    
+    public void nyontek() {
+        System.out.println("Mahasiswa sedang menyontek dalam ujian");
+    }
+    
+    public void modifikasi() {
+        System.out.println("Mahasiswa sedang memodifikasi tugas");
+    }
+}
