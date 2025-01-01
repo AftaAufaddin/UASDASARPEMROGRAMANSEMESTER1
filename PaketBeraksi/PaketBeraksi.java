a850e59e7f4f252a1d93594ee577d2aee4e6d409
AftaAufaddin <aftaaufaddin@gmail.com>
2025-01-01 18:38:21 +0700

    Add files via upload

---
+++ PaketBeraksi/PaketBeraksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PaketBeraksi;
+
+/**
+ *
+ * @author ADVAN
+ */
+import kelasku.*;
+        
+public class PaketBeraksi{
+    public static void main(String[] args){
+        Budi objectBudi = new Budi();
+	objectBudi.info();
+	Joko objectJoko = new Joko();
+	objectJoko.info();
+    }
+}
