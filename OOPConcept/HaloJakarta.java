5bf50bbb1f9d1ff3aadcb35f05f62761c09b32a4
AftaAufaddin <aftaaufaddin@gmail.com>
2025-01-01 18:18:23 +0700

    Add files via upload

---
+++ OOPConcept/HaloJakarta.java
@@ -0,0 +1,19 @@
+package OOPConcept;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ADVAN
+ */
+public class HaloJakarta {
+    
+    public static void main(String[] args) {
+        System.out.println("Halo Jakarta");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+    }
+}
